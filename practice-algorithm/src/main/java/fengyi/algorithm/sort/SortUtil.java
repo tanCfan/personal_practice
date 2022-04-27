df8a2f85e0756319db9424b455e72d09107573e8
fengyi <fengyi@51songguo.com>
2022-04-27 10:59:32 +0800

    bubble sort add comment

--- practice-algorithm/src/main/java/fengyi/algorithm/sort/SortUtil.java
+++ practice-algorithm/src/main/java/fengyi/algorithm/sort/SortUtil.java
@@ -19,8 +19,11 @@ public class SortUtil {
             return;
         }
 
+        //  N 个元素需要循环 N - 1 次才能确定所有位置元素
         for (int i = 0; i < array.length -1 ; i++) {
+            // 上一轮次循环已经确定位置的元素无需再在本次循环中再次参与比较
             for (int j = 0; j < array.length-1 - i; j++){
+                // 逐一比较相邻两个数的大小，如果当前位置比后一位置元素大则交换位置
                 if (array[j] > array[j+1]) {
                     int tem = array[j+1];
                     array[j+1] = array[j];
